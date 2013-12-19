678573a896af1b89dcd6721a78ad478edb4719c3
Dan Treasure <dtreasure@getjive.com>
2013-12-19 12:25:50 -0700

    Added a config file for allowing different setups, added a tests folder and file for testing configuration.

--- app/app.js
+++ app/app.js
@@ -5,10 +5,10 @@
 
 var express = require('express');
 var routes = require('./routes');
+var config = require('./config')();
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
 var app = express();
 
 // all environments
@@ -34,6 +34,6 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(config.port, function(){
+    console.log('Express server listening on port ' + config.port);
 });
